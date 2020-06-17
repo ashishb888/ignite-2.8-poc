f7be48b4255c83d5a840c2b1b4a1f49842e2c1fd
ashishb888 <ashish.bhosle008@gmail.com>
2020-06-17 17:05:02 +0530

    ignite-2.8-gs: Initial commit

---
+++ ignite-2.8-gs/src/main/java/poc/ignite/IgniteApp.java
@@ -0,0 +1,13 @@
+package poc.ignite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgniteApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgniteApp.class, args);
+	}
+
+}
