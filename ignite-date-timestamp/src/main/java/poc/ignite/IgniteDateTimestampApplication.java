0e221a26a4d2bfce14041f0179fea811a9621290
ashishb888 <ashish.bhosle008@gmail.com>
2020-08-10 13:11:13 +0530

    ignite-date-timestamp: Initial commit

---
+++ ignite-date-timestamp/src/main/java/poc/ignite/IgniteDateTimestampApplication.java
@@ -0,0 +1,13 @@
+package poc.ignite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgniteDateTimestampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgniteDateTimestampApplication.class, args);
+	}
+
+}
