cbf03b0704fdc3f614eed9b3e8175be13df1a97d
ashishb888 <ashish.bhosle008@gmail.com>
2020-10-09 17:47:34 +0530

    ignite-spring-resource: Initial commit

---
+++ ignite-spring-resource/src/main/java/poc/ignite/IgniteSpringResourceApplication.java
@@ -0,0 +1,13 @@
+package poc.ignite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgniteSpringResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgniteSpringResourceApplication.class, args);
+	}
+
+}
