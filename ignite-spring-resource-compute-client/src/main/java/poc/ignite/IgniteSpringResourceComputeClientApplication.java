bb018c33484ea081010ea5cd097af513f1bcd235
ashishb888 <ashish.bhosle008@gmail.com>
2020-10-10 10:27:58 +0530

    ignite-spring-resource-compute-client: Initial commit

---
+++ ignite-spring-resource-compute-client/src/main/java/poc/ignite/IgniteSpringResourceComputeClientApplication.java
@@ -0,0 +1,13 @@
+package poc.ignite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgniteSpringResourceComputeClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgniteSpringResourceComputeClientApplication.class, args);
+	}
+
+}
